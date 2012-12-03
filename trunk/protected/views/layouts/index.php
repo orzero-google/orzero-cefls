e1baea08997ff2f79cd65b50f8ac200a86e3ddd4
xami520@gmail.com <xami520@gmail.com@916062c6-69c9-8506-42d3-447dc2073e2e>
2012-12-03 08:19:25 +0000

    首页

---
+++ trunk/protected/views/layouts/index.php
@@ -0,0 +1,258 @@
+<!doctype html public "-//w3c//dtd html 4.01 transitional//en" "http://www.w3.org/tr/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html charset=utf8">
+    <title>首页</title>
+    <script type="text/javascript" src="/cefls/js/jquery.js"></script>
+    <script type="text/javascript" src="/cefls/js/script.js"></script>
+    <script type="text/javascript" src="/cefls/js/coin-slider.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/cefls/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="/cefls/css/coin-slider-styles.css" />
+    <link rel="stylesheet" href="/cefls/css/globle_cn.css" type="text/css" media="screen, project, print" />
+</head>
+<body>
+
+<!-- the header -->
+<div class="header">
+    <div class="header_top">
+        <div class="header_left"><a href="#"><img src="images/logo.jpg" width="101" height="101" alt="logo"/></a></div>
+        <div class="header_right">
+            <div class="header_title"><img src="images/index_r2_c11.jpg" width="404" height="101" alt="成都市实验外国语学校"/></div>
+            <div class="header_link">
+                <p class="header_link1"><a href="index.html">首页</a>　|　<a href="#">加入收藏</a>　|　<a href="#">联系我们</a>　|　<a href="#">实外信箱</a></p>
+                <p class="header_link2"><a href="#">ENGLISH</a>　|　<a href="#">Francais</a>　|　<a href="#">Deutsc</a></p>
+                <p class="header_link3"><input type="text" class="search_text"/><input type="submit" name="submit" class="submit"/></p>
+            </div>
+        </div>
+    </div>
+    <div class="hackbox"></div>
+    <div class="header_bottom">
+        <div class="header_nav">
+            <ul class="header_nav_link">
+                <li>
+                    <a href="#">关于实外</a>
+                    <ul>
+                        <li><a href="#">关于实外</a></li>
+                        <li><a href="#">二级标题</a></li>
+                        <li><a href="#">关于实外</a></li>
+                        <li><a href="#">二级标题</a></li>
+                        <li><a href="#">二级标题</a></li>
+                        <li><a href="#">二级标题</a></li>
+                        <li><a href="#">二级标题</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">师资队伍</a>
+                    <ul>
+                        <li><a href="#">师资队伍</a></li>
+                        <li><a href="#">师资队伍</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">德育视窗</a></li>
+                <li>
+                    <a href="#">艺体天地</a>
+                    <ul>
+                        <li><a href="#">艺体天地</a></li>
+                        <li><a href="#">艺体天地</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">教学科研</a></li>
+                <li><a href="#">外语特色</a></li>
+                <li><a href="#">交流合作</a></li>
+                <li><a href="#">感悟实外</a></li>
+                <li><a href="#">学子风采</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="hackbox"></div>
+
+<!-- index's slide  -->
+<div class="videoList-container" id="pcList" style="display:none;margin-bottom:0px;">
+    <div id="slide-index">
+        <div class="slides">
+            <div class="slide autoMaxWidth" links=[{left:'30px',top:'81px'},{left:'30px',top:'244px'},{direction:'tb'}]>
+            <div class="image" id='bi_0'><a target="_blank" href="#"><img name="" src="images/hw_149403.jpg" /></a></div>
+            <div class="text" id='bt_0'></div>
+            <div class="button" id='bb_0'></div>
+        </div>
+        <div class="slide autoMaxWidth" links=[{left:'30px',top:'81px'},{left:'30px',top:'244px'},{direction:'tb'}]>
+        <div class="image" id='bi_1'><a href="#" target="_blank"><img name="" src="images/hw_149025.jpg" /></a></div>
+        <div class="text" id='bt_1'></div>
+        <div class="button" id='bb_1'></div>
+    </div>
+    <div class="slide autoMaxWidth" links=[{left:'30px',top:'81px'},{left:'30px',top:'244px'},{direction:'tb'}]>
+    <div class="image" id='bi_2'><a target="" href="#"><img name="" src="images/hw_194667.jpg" /></a></div>
+    <div class="text" id='bt_2'></div>
+    <div class="button" id='bb_2'></div>
+</div>
+</div>
+<div class="control">
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+</div>
+</div>
+</div>
+<script type="text/javascript" src="./js/global_cn.index.js"></script>
+<!-- index's newsPlan  -->
+<div class="newsPlan">
+    <div class="left">
+        <div class="leftImg"><img src="images/3.jpg" alt="校长照片" width="151px" height="146px"></div>
+        <div class="leftTitle">成都市实验外国语学校<br/>校长：温瑞征</div>
+        <div class="leftDescribe">
+            温瑞征，成都市实验外国语学校创始人 “四川省教育风云人物”，成都市优秀 学校长，成都市优秀教育工作者，一位 著教育四十余年的.....
+            <p><a href="#">『详细内容』</a></p>
+        </div>
+    </div>
+    <div class="middle">
+        <div class="middleTop"><span><img src="images/index_r11_c19.jpg" alt="更多"></span></div>
+        <div class="middleMiddle">
+            <div class="content">
+                <div class="cLeft">
+                    <h3 class="title"><a href="#">我校第九届活动周顺利开幕</a></h3>
+                    <p class="describe">中国新闻网是知名的中文新闻门户网站，也是全球互联网中文新闻资讯最重要的原创内容供应商之一。年轻向上的力量。是全方位的新闻资讯平台，24小时为您滚动报道国内、依托中新社遍布全球的采编网络,每天24小时面向广大网民 ...</p>
+                </div>
+                <div class="cRight"><a href="#"><img src="images/5.jpg" width="177px" height="128px" alt="我校第九届活动周顺利开幕" /></a></div>
+            </div>
+        </div>
+        <div class="middleBottom">
+            <p><span>2012-11-20</span><a href="#">Apache与IIS的优劣对比点点评</a></p>
+            <p><span>2012-11-30</span><a href="#">[配置]MRTG＋IIS 6＝接近完美的信息监控！</a></p>
+            <p><span>2012-11-21</span><a href="#">IIS6应用程序池中间的 Web 园</a></p>
+            <p><span>2012-11-24</span><a href="#">中国商务部今日发表的统计报告</a></p>
+            <p><span>2012-11-25</span><a href="#">《世界新闻报·鉴赏中国专刊》介绍. 创新. • 环保华服</a></p>
+        </div>
+    </div>
+    <div class="right">
+        <div class="content">
+            <h3 class="title"><a href="#">高中录取分数线</a></h3>
+            <p class="describe">一、我校初中毕业学生录取线93分及以上，奖学金线在学校查询；二、非本校初中毕业生660分及以上（免交学费）；三、凡上线生报名时间为6月29日下午和6月30日上午11点30分前，过期不再录取。成都市实验外国语学校2012年6月29日 </p>
+            <p class="link"><a href="#">『查看更多』</a></p>
+            <script type="text/javascript">
+
+            </script>
+        </div>
+    </div>
+</div>
+
+<!-- index's adList  -->
+<div class="adList">
+    <div class="ad1"><a href="#"><img src="images/ad1.jpg" alt="ad1"></a></div>
+    <div class="ad2"><a href="#"><img src="images/ad2.jpg" alt="ad2"></a></div>
+    <div class="ad3"><a href="#"><img src="images/ad3.jpg" alt="ad3"></a></div>
+</div>
+
+<!-- index's foreign -->
+<div class="foreignPlan">
+    <div class="top">
+        <ul>
+            <li class="hover">English</li>
+            <li>Francais</li>
+            <li>Deutsh</li>
+        </ul>
+    </div>
+    <div class="bottom">
+        <div class="bLeft">
+            <div class="brief">
+                <div class="blbTop">CEFLS  Profile</div>
+                <div class="blbBottom">Chengdu Experimental Foreign Languages SchoolS) is a nationwide famous foreign languages scne directly under the Chengdu unicipal Educat mittee. Located at No.134 Section 1 N. Yihuan Chengdu City. The school covers an area of 60 unded in 1963, the original Chengdu No.48 Middol was renamed Chengdu Experimental Foreign La School in 1993 with the approva......</div>
+            </div>
+            <div class="brief">
+                <div class="blbTop">CEFLS  Culture</div>
+                <div class="blbBottom">Chengdu Experimental Foreign Languages SchoolS) is a nationwide famous foreign languages scne directly under the Chengdu unicipal Educat mittee. Located at No.134 Section 1 N. Yihuan Chengdu City. The school covers an area of 60 unded in 1963, the original Chengdu No.48 Middol was renamed Chengdu Experimental Foreign La School in 1993 with the approva......</div>
+            </div>
+        </div>
+        <div class="bMiddle">
+            <div class="bothSide">
+                <div class="newsPic">
+                    <img src="images/6.jpg" width="150px" height="106px" alt="">
+                    <p><a href="#">This is picture's title information</a></p>
+                </div>
+                <div class="newsPic">
+                    <img src="images/6.jpg" width="150px" height="106px" alt="">
+                    <p><a href="#">This is picture's title information</a></p>
+                </div>
+                <div class="newsPic">
+                    <img src="images/6.jpg" width="150px" height="106px" alt="">
+                    <p><a href="#">This is picture's title information</a></p>
+                </div>
+            </div>
+            <div class="middle">
+                <div class="recommend">
+                    <h3><a href="#">Foreign Languages School</a></h3>
+                    <p>CDEFLS has made great academic achievements.s the junior middle school students have rakby exam results in Chengdu. 100% of the CDEL graduates have passed .</p>
+                </div>
+                <div class="list">
+                    <p><span>(11-20)</span><a href="#">CEFLS Qualified for Beijing University’s</a></p>
+                    <p><span>(11-30)</span><a href="#">Remarkable Achievements in 27th Nationa...</a></p>
+                    <p><span>(11-21)</span><a href="#">Friendly Visit from Switzerland</a></p>
+                    <p><span>(11-24)</span><a href="#">2010 Autumn-term Opening Ceremony held</a></p>
+                    <p><span>(11-25)</span><a href="#">Students Awarded in Financial Knowledge...</a></p>
+                    <p><span>(11-25)</span><a href="#">Remarkable Achievements in NEPCSS</a></p>
+                    <p><span>(11-25)</span><a href="#">CEFLS Accepted as Research Base by NAFLE</a></p>
+                    <p><span>(11-30)</span><a href="#">Remarkable Achievements in 27th Nationa...</a></p>
+                    <p><span>(11-21)</span><a href="#">Friendly Visit from Switzerland</a></p>
+                    <p><span>(11-24)</span><a href="#">2010 Autumn-term Opening Ceremony held</a></p>
+                </div>
+            </div>
+            <div class="bothSide">
+                <div class="newsPic">
+                    <img src="images/6.jpg" width="150px" height="106px" alt="">
+                    <p><a href="#">This is picture's title information</a></p>
+                </div>
+                <div class="newsPic">
+                    <img src="images/6.jpg" width="150px" height="106px" alt="">
+                    <p><a href="#">This is picture's title information</a></p>
+                </div>
+                <div class="newsPic">
+                    <img src="images/6.jpg" width="150px" height="106px" alt="">
+                    <p><a href="#">This is picture's title information</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom" style="dispaly:none;">法语</div>
+    <div class="bottom" style="dispaly:none;">荷兰语</div>
+    <script type="text/javascript">
+        $(function(){
+            var $title = $(".foreignPlan .top li");
+            var $content = $(".foreignPlan .bottom");
+            $title.mousemove(function(){
+                var index = $title.index($(this));
+                $title.removeClass("hover");
+                $(this).addClass("hover");
+                $content.hide();
+                $($content.get(index)).show();
+
+                return false;
+            });
+        });
+    </script>
+</div>
+<div class="hackbox"></div>
+
+<!-- index's frindLinks -->
+<div class="frindLinks">
+    <div class="linkList">
+        <a href="#">成都外国语学校</a>
+        <a href="#">成都市实验外国语学校（西区）</a>
+        <a href="#">四川外语学院成都学院</a>
+        <a href="#">成外附属小学</a>
+        <a href="#">四川省计算机学会</a>
+        <a href="#">成都市实验外国语学校（西区）</a>
+        <a href="#">成都外国语学校               </a>
+        <a href="#">四川省计算机学会</a>
+        <a href="#">四川外语学院成都学院</a>
+        <a href="#">成外附属小学</a>
+    </div>
+</div>
+
+<!-- index's footer -->
+<div class="footer">
+    <p><a href="#">关于我们</a>  ‖  <a href="#">网站声明</a>  ‖ <a href="#">实外信箱</a>  ‖ <a href="#">联系我们</a></p>
+    <p>网站版权所有者：成都市实验外国语学校    协同建设支持单位：成都互正超媒网络科技有限公司<br/>Copyright©2011-2016  (www.cefls.cn )    All Right Reserved <br/>川教GZ-20120014号       蜀ICP备12003383-1号</p>
+</div>
+</body>
+</html>
