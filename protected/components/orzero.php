e797d97563bb2fb56894282378c105a80fcf185d
xami520@gmail.com <xami520@gmail.com@916062c6-69c9-8506-42d3-447dc2073e2e>
2012-12-04 10:07:54 +0000

    更新menu数据

--- protected/components/orzero.php
+++ protected/components/orzero.php
@@ -5,3 +5,16 @@
  * Time: 下午5:13
  *
  */
+
+
+function get_menu(){
+    $menus = Menu::model()->with('sub_menu')->find('t.menu_type=:menu_type',array(':menu_type'=>1));
+    print_r($menus);
+    foreach($menus as $menu_one){
+
+        foreach($menu_one->sub_menu as $sub_menu){
+
+        }
+    }
+
+}
