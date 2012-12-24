6337b88028d76b395fa795c8dcd540cea97a9d40
xami520@gmail.com <xami520@gmail.com@916062c6-69c9-8506-42d3-447dc2073e2e>
2012-12-24 16:00:46 +0000

    学生风采

--- protected/views/cefls/ads/student_list.php
+++ protected/views/cefls/ads/student_list.php
@@ -1,67 +1,42 @@
+<?php
+$criteria=new CDbCriteria;
+$criteria->condition='`cid`=:cid AND `type`=-10 AND `enabled`=1';
+$criteria->params=array(':cid'=>$cid);
+$criteria->order='`order` ASC, `aid` DESC';
+$dataProvider=new CActiveDataProvider('Ads',array(
+    'criteria'=>$criteria,
+    'pagination'=>array(
+        'pageSize'=>4,
+    ),
+));
+
+?>
+
 <div class="top"></div>
 <div class="middle">
-    <div class="numberOne">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <div class="numberOne">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <div class="numberOne">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <div class="numberOne">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <!-- 加autohidden为默认隐藏 -->
-    <div class="numberOne autohidden">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <div class="numberOne autohidden">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <div class="numberOne autohidden">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
-    <div class="numberOne autohidden">
-        <a href="#"><img src="images/10.jpg" width="280px" height="209px" alt=""></a>
-        <div class="info">
-            <p><a href="#">旻天郝</a></p>
-            <p>2012四川省高考文科状元</p>
-        </div>
-    </div>
+    <?php
+    $this->widget('application.vendors.OListView', array(
+        'dataProvider'=>$dataProvider,
+        'itemView'=>'//cefls/ads/student_one',
+        'ajaxUpdate'=>true,
+        'template'=>"{items}" ,
+        'itemsTagName'=>'ul',
+//                    'htmlOptions'=>array('class'=>'thingsList'),
+    ));
+    ?>
     <div class="hackbox"></div>
 </div>
 <div class="bottom" id="bottom">
+    <?php
+//    $this->widget('application.vendors.OListView', array(
+//        'dataProvider'=>$dataProvider,
+//        'itemView'=>'//cefls/ads/student_one',
+//        'ajaxUpdate'=>true,
+//        'template'=>"{pager}"
+//    ));
+    ?>
     <div class="numberOneShow">
-        <a href="#bottom"><img src="images/container_r8_c9.jpg" alt=""></a>
+        <a href="#bottom"><img src="/cefls/images/container_r8_c9.jpg" alt=""></a>
     </div>
     <script type="text/javascript">
         $(document).ready(function(){
