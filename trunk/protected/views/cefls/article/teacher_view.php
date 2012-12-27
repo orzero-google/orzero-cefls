9367e4df8063bafece9093a1e35c4e02ec8940f5
xami520@gmail.com <xami520@gmail.com@916062c6-69c9-8506-42d3-447dc2073e2e>
2012-12-27 15:59:55 +0000

    教师详情

--- trunk/protected/views/cefls/article/teacher_view.php
+++ trunk/protected/views/cefls/article/teacher_view.php
@@ -1,4 +1,9 @@
+<?php
+$pid=Yii::app()->request->getParam('pid', 0);
+$cid=Yii::app()->request->getParam('cid', 0);
+$tid=Yii::app()->request->getParam('tid', 0);
+?>
 <div class="imgitem">
-    <a href="#" width="150" height="210"><img src="<?php echo $data->file; ?>" width="150" height="210" alt=""></a>
-    <p><a href="#">副校长：某某某</a></p>
+    <a href="<?php echo $this->createUrl('cate/index', array('pid'=>$pid, 'cid'=>$cid, 'tid'=>$data->aid)); ?>" width="150" height="210"><img src="<?php echo $data->file; ?>" width="150" height="210" alt=""></a>
+    <p><a href="<?php echo $this->createUrl('cate/index', array('pid'=>$pid, 'cid'=>$cid, 'tid'=>$data->aid)); ?>"><?php echo $data->title; ?>：<?php echo $data->author; ?></a></p>
 </div>
