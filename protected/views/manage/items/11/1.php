e4f6ed38b68cc95c510d061c6c6622f4bdc8300d
xami520@gmail.com <xami520@gmail.com@916062c6-69c9-8506-42d3-447dc2073e2e>
2013-01-05 09:55:29 +0000

    get_list_article()

--- protected/views/manage/items/11/1.php
+++ protected/views/manage/items/11/1.php
@@ -7,11 +7,11 @@
  * .
  */
 
-$cid = -1;
+//$cid = Yii::app()->request->getParam('cid', 0);
 $time = date("Y-m-d H:i:s");
 
 $id=Yii::app()->request->getParam('id', 0);
-$model = Article::model()->article_list($cid)->findByPk($id);
+$model = Article::model()->open()->findByPk($id);
 
 if(empty($model)){
     $model = new Article;
@@ -20,14 +20,14 @@ if(empty($model)){
 }
 
 $model->uid=Yii::app()->user->id;
-$model->cid=$cid;
+//$model->cid=$cid;
 $model->enabled=1;
 $model->audit=1;
 $model->grade=0;
 $model->createtime=empty($model->createtime) ? $time : $model->createtime;
 $model->updatetime=$time;
 $model->sort=0;
-$model->type=0;
+$model->type=-11;
 
 if(isset($_POST['Article']))
 {
@@ -40,7 +40,7 @@ if(isset($_POST['Article']))
     }
 
     if($model->save())
-        $this->redirect(array('manage/index', 'pid'=>4, 'cid'=>2));
+        $this->redirect(array('manage/index', 'pid'=>11, 'cid'=>2));
 }
 
 echo $this->renderPartial('//cefls/article/list_article_add', array('model'=>$model));
