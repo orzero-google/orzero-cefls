8092734720ee86f6f7c61d53bb2f92bd15bed517
xami520@gmail.com <xami520@gmail.com@916062c6-69c9-8506-42d3-447dc2073e2e>
2013-03-04 12:15:12 +0000

    领导班子html

--- protected/views/cefls/article/teacher_one.php
+++ protected/views/cefls/article/teacher_one.php
@@ -1,11 +1,11 @@
 <?php
 if(!empty($teacher)) :
 ?>
-<div class="top"></div>
 <div class="middle">
-    <p class="teacherDetail" style="margin-left: 20px;">
-        <img src="<?php echo isset($teacher->file) ? $teacher->file:''; ?>" alt="" style="float:left;margin:5px;display: inline-block;width: 200px;">
-        <?php echo isset($teacher->content) ? strip_tags($teacher->content): ''; ?>
+    <p class="teacherDetail" style="width:auto;margin-left: -20px;">
+<!--        <img src="--><?php //echo isset($teacher->file) ? $teacher->file:''; ?><!--" alt="" style="float:left;margin:5px;display: inline-block;width: 200px;">-->
+<!--        --><?php //echo isset($teacher->content) ? strip_tags($teacher->content): ''; ?>
+    <iframe frameborder="0" id="Article_content_ifr" src="/index.php/cate/article_one?id=<?php echo isset($teacher->aid) ? $teacher->aid: ''; ?>" allowtransparency="true" style="width: 800px; min-height: 800px;"></iframe>
     </p>
     <p class="teacherDetailInfo">点击量：<?php echo isset($teacher->clicknumber) ? $teacher->clicknumber : '';?></p>
 </div>
